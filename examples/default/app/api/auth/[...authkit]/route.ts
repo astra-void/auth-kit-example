54b8ee1559d9d1b8db0079c2aeecad456cb542f9
astra-void <syntaxalchemist0@gmail.com>
2025-07-04 08:43:18 +0900

    magiclink

--- examples/default/app/api/auth/[...authkit]/route.ts
+++ examples/default/app/api/auth/[...authkit]/route.ts
@@ -1,9 +1,13 @@
 import { prisma } from "@/app/lib/prismadb";
 import { AuthKit } from "@astra-void/auth-kit";
 import { PrismaAdapter } from "@astra-void/auth-kit/adapters";
+import { CredentialsProvider } from "@astra-void/auth-kit/providers";
  
 const handler = AuthKit({
   adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider(),
+  ],
 });
  
 export { handler as GET, handler as POST };
